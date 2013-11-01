0949e071df2b6cd9c04266ba6b7bd82ecf18e731
Ernesto Medina <edelgado@thoughtworks.com>
2013-11-01 16:05:19 -0500

    First commit basic Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeelmeServer::Application.config.secret_key_base = 'c923f83089cf2f3239679182e6d9bf46318e3d2fd58fa96cbd0e6fea6c21225c1afb46c0d4f9105dde0ec3271db448889ad2cdb52c244d101f6e53195208b315'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FeelmeServer::Application.config.session_store :cookie_store, key: '_feelme_server_session'
