4e7cf11879cc602c0908134b0b156488d53dd89a
Mercy Falconi <mfalconi@thoughtworks.com>
2013-11-15 11:22:19 -0500

    [Rebecca-Mercy] Aceptance test for pie update

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
 Given /^I am on (.+)$/ do |page_name|
-  Capybara.app_host = 'http://0.0.0.0:4000'
   @session = Capybara::Session.new(:selenium)
   @session.visit path_to(page_name)
   sleep(2)
