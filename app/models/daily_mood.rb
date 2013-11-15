db81218ce6406294165a3b47c72e510f3c1c836c
Ernesto Medina <edelgado@thoughtworks.com>
2013-11-15 10:31:06 -0500

    Ernesto/Rodrigo Implements method today for DailyMood

--- app/models/daily_mood.rb
+++ app/models/daily_mood.rb
@@ -2,4 +2,6 @@ class DailyMood < ActiveRecord::Base
   validates :date, presence: true, uniqueness: true
   validates :happy_count, presence: true, numericality: true
   validates :unhappy_count, presence: true, numericality: true
+
+  scope :today, -> { where("DATE(date) = ?", Date.today).first }
 end
--- spec/models/daily_mood_spec.rb
+++ spec/models/daily_mood_spec.rb
@@ -1,10 +1,26 @@
 require "spec_helper"
 
 describe DailyMood do
-  it { should validate_presence_of(:date) }
-  it { should validate_presence_of(:happy_count) }
-  it { should validate_presence_of(:unhappy_count) }
-  it { should validate_numericality_of(:happy_count)}
-  it { should validate_numericality_of(:unhappy_count)}
-  it { should validate_uniqueness_of(:date)}
+  describe "validations" do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:happy_count) }
+    it { should validate_presence_of(:unhappy_count) }
+    it { should validate_numericality_of(:happy_count)}
+    it { should validate_numericality_of(:unhappy_count)}
+    it { should validate_uniqueness_of(:date)}
+  end
+
+  describe "scopes" do
+    it "should return last daily mood by date" do
+      today = DateTime.now
+      yesterday = today - 1.day
+      today_mood = DailyMood.create(happy_count: 6,
+                                    unhappy_count: 4,
+                                    date: today)
+      yesterday_mood = DailyMood.create(happy_count: 7,
+                                        unhappy_count: 5,
+                                        date: yesterday)
+      DailyMood.today.should == today_mood
+    end
+  end
 end
