bea30845861364491bb305b57e7cdf46bd34f8d6
Ernesto Medina <edelgado@thoughtworks.com>
2013-11-27 11:19:21 -0500

    Adding date to view model with default value

--- app/models/daily_mood_view_model.rb
+++ app/models/daily_mood_view_model.rb
@@ -3,8 +3,16 @@ class DailyMoodViewModel
   include ActiveModel::Conversion
   include ActiveModel::Naming
 
+  attr_reader :daily_mood
+
+  delegate :date=, :persisted?, to: :daily_mood
+
   def initialize(daily_mood, moods)
     @daily_mood = daily_mood
     @moods = moods
+  end
+
+  def date
+    @daily_mood.date ||= DateTime.now
   end
 end
