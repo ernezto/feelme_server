6a71d02b7af794bdb858d2a9d3f56839c9a25331
Ernesto Medina <edelgado@thoughtworks.com>
2013-11-28 16:28:36 -0500

    Added final implementation for vm

--- app/controllers/daily_moods_controller.rb
+++ app/controllers/daily_moods_controller.rb
@@ -1,12 +1,14 @@
 class DailyMoodsController < ApplicationController
   def new
-    @daily_mood_vm = DailyMoodViewModel.new(Mood.all)
+    @daily_mood_vm = DailyMoodViewModel.new()
+    @moods = Mood.all
   end
 
   def create
-    @daily_mood_vm = DailyMoodViewModel.new(Mood.all)
-    @daily_mood_vm.attributes = params[:daily_mood_view_model]
-    if @daily_mood_vm.validate!
+    p daily_mood_params
+    @moods = Mood.all
+    @daily_mood_vm = DailyMoodViewModel.new(daily_mood_params)
+    if @daily_mood_vm
       @daily_mood_vm.save
       flash.now[:success] = "Registered!"
       redirect_to action: "new"
@@ -25,13 +27,13 @@ class DailyMoodsController < ApplicationController
   end
 
   def current_week
-    @current_week_moods = DailyMood.current_week 
+    @current_week_moods = DailyMood.current_week
 
     render formats: [:json]
   end
 
   private
   def daily_mood_params
-    params.require(:daily_mood_view_model).permit(:moods, :date)
+    params.require(:daily_mood_view_model)#.permit(:daily_moods, :date)
   end
 end
--- app/models/daily_mood_view_model.rb
+++ app/models/daily_mood_view_model.rb
@@ -3,42 +3,32 @@ class DailyMoodViewModel
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_reader :date, :moods, :errors
+  attr_reader :date, :daily_moods, :errors
 
-  def initialize(moods)
-    @moods = moods
+  def initialize(params = nil)
     @date = DateTime.now
-    @save_list = []
     @errors = ActiveModel::Errors.new(self)
-  end
-
-  def attributes=(attributes)
-    moods = attributes[:moods] || []
-    moods.each do |k, v|
-      @save_list << DailyMood.new(date: attributes[:date], count: v, mood_id: k)
-    end
+    @daily_moods = []
+    mapping params
   end
 
   def persisted?
-    @save_list.count == @moods.count &&
-   !@save_list.map(&:persisted?).include?(false)
-  end
-
-  def validate!
-    puts "Validates runs"
-    @errors.add(:moods, "Should be more than one") if @moods == nil || @moods.count == 0
-    @errors.add(:date, "Can not be nil") if @date == nil
-    @save_list.each do |daily_mood|
-      @errors.add(:mood, "Can not be nil") if daily_mood.count == nil
-    end
-    errors.count == 0
+    false
   end
 
   def save
     DailyMood.transaction do
-      @save_list.each do |daily_mood|
-        daily_mood.save!
+      @daily_moods.each do |daily_mood|
+        daily_mood.save
       end
     end
+  end
+
+  private
+  def mapping(params)
+    return if params == nil
+    moods = params[:daily_moods] || []
+    moods.each { |m| @daily_moods << DailyMood.new(m) }
+    @daily_moods.each{ |dm| dm.date = params[:date] }
   end
 end
